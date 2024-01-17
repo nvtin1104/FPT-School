f4b0419329d684a87629df4d1c95e6702d040043
Nguyen Van Tin <bguyt46@gmail.com>
2024-01-17 21:10:21 +0700

    update

--- HocKySummer2024/jsNangCao/Lab/Lab2/lab2.js
+++ HocKySummer2024/jsNangCao/Lab/Lab2/lab2.js
@@ -159,3 +159,12 @@ for (const flight of flights.split('+')) {
     console.log(output);
 }
 
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
+})();
+
