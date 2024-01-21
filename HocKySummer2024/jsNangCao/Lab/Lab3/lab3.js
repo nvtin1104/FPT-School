0f92f2bd7eb735cd13d3ffc0d6a7ff6ecf263ad1
Nguyen Van Tin <bguyt46@gmail.com>
2024-01-21 22:39:02 +0700

    update

--- HocKySummer2024/jsNangCao/Lab/Lab3/lab3.js
+++ HocKySummer2024/jsNangCao/Lab/Lab3/lab3.js
@@ -13,3 +13,28 @@ const checkDogs = (dogsJulia, dogsKate) => {
 }
 checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+//
+console.log('==============YC3.3===============');
+const calcAverageHumanAge = function (dogs) {
+    console.log('---Tinh tuoi---');
+    const age = dogs.map((dog) => {
+        if (dog > 2) {
+            return 16 + dog * 4;
+        }
+        else {
+            return dog * 2;
+        }
+    })
+    console.log('Tính tuổi của chó theo tuổi của con người', age);
+    console.log('---Loc---');
+    const ageFilter = age.filter(dog => dog > 18);
+    console.log(' Loại trừ tất cả những chú chó có tuổi nhỏ hơn so với 1 người 18 tuổi', ageFilter);
+    console.log('---Tinh trung binh tuoi---');
+    const sumAge = ageFilter.reduce((acc, cur) => acc + cur, 0);
+    const absAge = sumAge / age.length;
+    console.log('Tính tuổi trung bình của các chú chó trưởng thành ra tuổi người', absAge);
+}
+console.log('Test data 1');
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+console.log('Test data 2');
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
